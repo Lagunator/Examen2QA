a76188530041acf907b10a1abf4008af57e59a95
¨Lagunator¨ <jhosisasmauricio@gmail.com>
2023-10-18 12:13:20 -0400

    codigo listo de clases

---
+++ src/test/java/api/config/Configuration.java
@@ -0,0 +1,8 @@
+package api.config;
+
+public class Configuration {
+    public static String user="lagu@lagu.com";
+    public static String password="12345678";
+
+    public static String host="https://todo.ly";
+}
